2ba18dfda70144765c1d05586ec2c761f819c23f
Cassyfok <sweetkc_fok@yahoo.com>
2014-02-26 16:57:40 +0000

    testing

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -3,9 +3,10 @@ class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 	
 	public function index() {
+		//finding all the records in the Post table & handing the response to the index.ctp//
         $this->set('posts', $this->Post->find('all'));
     }
-	
+
 	public function view($id = null) {
         if (!$id) {
             throw new NotFoundException(__('Invalid post'));
@@ -17,5 +18,73 @@ class PostsController extends AppController {
         }
         $this->set('post', $post);
     }
+	//this function will allow us to add to the posts database//
+	public function add() {
+		//checking if this is a HTTP post request//
+        if ($this->request->is('post')) {
+			//initializing the Post Model//
+            $this->Post->create();
+			//handing the request object data to be saved by the Post Model//
+            if ($this->Post->save($this->request->data)) {
+				//flashing a success message//
+                $this->Session->setFlash(__('Your post has been saved.'));
+				//redirecting to the Posts index action//
+                return $this->redirect(array('action' => 'index'));
+            }
+			//flashing a message saying we weren't able to do it//
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+    }
+	
+	//function will allow us to edit an existing post//
+		public function edit($id = null) {
+    if (!$id) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    $post = $this->Post->findById($id);
+    if (!$post) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+	
+	//to check if is a post request or put request//
+    if ($this->request->is(array('post', 'put'))) {
+        $this->Post->id = $id;
+        if ($this->Post->save($this->request->data)) {
+            $this->Session->setFlash(__('Your post has been updated.'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Unable to update your post.'));
+    }
+	
+    if (!$this->request->data) {
+        $this->request->data = $post;
+    }
+}
+	
+	//send id to the posts that need to be deleted//
+	public function delete($id) {
+		//if is HTTP get request don't allow//
+		if ($this->request->is('get')) {
+			throw new MethodNotAllowedException();
+		}
+		//if the post is been deleted//
+		if ($this->Post->delete($id)) {
+			$this->Session->setFlash(
+				__('The post with id: %s has been deleted.', h($id))
+			);
+			return $this->redirect(array('action' => 'index'));
+		}
+	}
+
 }
 ?>
+
+
+
+
+
+
+
+
+
--- app/View/Posts/index.ctp
+++ app/View/Posts/index.ctp
@@ -15,10 +15,31 @@
         <td><?php echo $post['Post']['id']; ?></td>
         <td>
             <?php echo $this->Html->link($post['Post']['title'],
-array('controller' => 'posts', 'action' => 'view', $post['Post']['id'])); ?>
+				array('controller' => 'posts', 'action' => 'view', $post['Post']['id'])); ?>
         </td>
-        <td><?php echo $post['Post']['created']; ?></td>
+		<td><?php echo $post['Post']['created']; ?></td>
+		<td>
+			<?php
+                echo $this->Form->postLink(
+                    'Delete',
+                    array('action' => 'delete', $post['Post']['id']),
+                    array('confirm' => 'Are you sure?')
+                );
+            ?>
+            <?php
+                echo $this->Html->link(
+                    'Edit',
+                    array('action' => 'edit', $post['Post']['id'])
+                );
+            ?>
+        </td>
+     
     </tr>
     <?php endforeach; ?>
     <?php unset($post); ?>
 </table>
+
+<?php echo $this->Html->link(
+    'Add Post',
+    array('controller' => 'posts', 'action' => 'add')
+); ?>
